54ef7946bd080dba953f635761c013a9ec0979b6
cellistigs <ta2507@columbia.edu>
2020-02-07 18:49:05 -0500

    restructured user profiles. scripts now run location independently, and new directory creation is automated.

---
+++ ncap_iac/user_profiles/iac_utils/configure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+### Script that automates the creation of new user profile templates. 
+set -e
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ncaprootdir="$(dirname "$(dirname "$scriptdir")")"
+
+source "$scriptdir"/paths.sh
+
+source activate sam
+
+## Input management: 
+## Get the path to the directory where user data is stored: 
+[ "$#" == 1 ] || { echo "ERROR: input is template name to be created"; exit; }
+## Check the name is valid. 
+python checkpath.py "$1"
+
+#Now make a directory: 
+cd "$ncaprootdir"/user_profiles
+mkdir "$1"
+cp user_config_template.json "$ncaprootdir"/user_profiles/"$1"/user_config_template.json 
+
+
+
---
+++ ncap_iac/user_profiles/user-stack-us-east-1-2/compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+source activate sam
+
+## Get the directory name: 
+PIPEDIR=$PWD
+## This can double as the stack name: 
+
+PIPENAME=$(basename "$PIPEDIR")
+
+
+cd ../template_utils
+python user_maker.py $PIPEDIR/user_config_template.json 
+
+
+cd $PIPEDIR
+
+sam build -t compiled_users.json -m ../lambda_repo/requirements.txt
+
+sam package --s3-bucket ctnsampackages --output-template-file compiled_users.yaml
+
+sam deploy --template-file compiled_users.yaml --stack-name $PIPENAME --capabilities CAPABILITY_NAMED_IAM
+
+## Added February 4th:
+cd ../template_utils
+python export_credentials.py $PIPEDIR/user_config_template.json 
+
+########
