12e2c814b6ccb95d51dd241a7fde8b3db92c196a
cellistigs <ta2507@columbia.edu>
2020-03-20 14:48:49 -0400

    figuring out build.

--- ncap_iac/ncap_blueprints/iac_utils/build.sh
+++ ncap_iac/ncap_blueprints/iac_utils/build.sh
@@ -11,7 +11,6 @@ source activate sam
 
 ## Input management: 
 ## Get the path to the directory where user data is stored: 
-echo $1
 [ -d "$1" ] || { echo "ERROR: Must give path to analysis stack directory"; exit; }
 
 PIPEDIR=$(get_abs_filename "$1")
@@ -26,7 +25,7 @@ python "$scriptdir"/checkpath.py "$PIPENAME"
 
 ## Give the path to the root directory for ncap (we like absolute paths) 
 
-cd $ncaprootdir/ncap_blueprints/template_utils
+cd $ncaprootdir/utils
 ## Run different deployment scripts based on version:
 version=$(jq ".PipelineVersion" "$PIPEDIR"/stack_config_template.json ) 
 versint=$(echo $version | tr -d '"')
@@ -45,5 +44,5 @@ fi
 
 ## We need to navigate to the pipeline directory because we have a relative path in our compilation code. 
 cd $PIPEDIR
-
-sam build -t compiled_template.json -m "$ncaprootdir"/ncap_blueprints/lambda_repo/requirements.txt
+echo $ncaprootdir "rootdir"
+sam build -t compiled_template.json -m "$ncaprootdir"/protocols/requirements.txt
