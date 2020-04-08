a735731e5bed861bfd7da4bd2bb9b1396712cb20
cellistigs <ta2507@columbia.edu>
2020-04-08 12:45:09 -0400

    system tests for deployed pipelines under latest infrastructure configuration.

--- ncap_iac/tests/integration_tests/dev_tests/test_1_1_under_duration.py
+++ ncap_iac/tests/integration_tests/dev_tests/test_1_1_under_duration.py
@@ -1,6 +1,16 @@
 import os 
 import json 
 import boto3 
+import pathlib
+import time
+from botocore.exceptions import ClientError
+
+
+## Get global parameters:
+utildir = pathlib.Path(__file__).parent.absolute()
+basedir = os.path.dirname(os.path.dirname(os.path.dirname(utildir)))
+with open(os.path.join(basedir,"global_params.json")) as gp:
+    gpdict = json.load(gp)
 
 def test_standard_case():
     s3_client = boto3.client("s3")
@@ -50,12 +60,52 @@ def test_standard_case():
     submit = {}
     submit["dataname"] = datauploadpath
     submit["configname"] = configuploadpath
-    submit["timestamp"] = "11:04:11"
+    submit["timestamp"] = "11:04:112"
     ## then submit it. 
     dataobj = s3_resource.Object(bucketname,os.path.join(groupname,"submissions","test11underdurationsubmit.json"))
     dataobj.put(Body = (bytes(json.dumps(submit).encode("UTF-8"))))
 
     ## Now we wait for the test session to complete. We can get a rough idea of when this will be based on the duration parameter in the config. [the ami we have for the instance ]
+    with open(configfilepath,"r") as cf:
+        cfdict = json.load(cf)
+        waittime = int(cfdict["wait"])
+
+    time.sleep(waittime+200)
+    ## check that the appropriate results folder exists. 
+    resultlogpath = os.path.join(groupname,"results","job__epi-ncap-stable_{}".format(submit["timestamp"]),"logs")
+    try:
+        out = s3_client.list_objects_v2(Bucket=bucketname,Prefix = resultlogpath)
+        contents = out["Contents"]
+        keys = [zi["Key"] for zi in contents]
+        ## First check for existence of logs
+        dataset_status = os.path.join(resultlogpath,"DATASET_NAME:dataset1.txt_STATUS.txt")
+        result_status = os.path.join(resultlogpath,"certificate.txt")
+        assert dataset_status in keys
+        assert result_status in keys
+        
+        ## Read in the Dataset status: 
+        file_object = s3_resource.Object(bucketname,dataset_status)
+        raw_content = file_object.get()['Body'].read().decode('utf-8')
+        dsdict = json.loads(raw_content)
+        assert dsdict["status"] in ["SUCCESS","INITIALIZING"]
+        instanceid = dsdict["instance"]
+        ## Now go get the corresponding log file:
+        print(instanceid)
+        log_object = s3_resource.Object(bucketname,os.path.join("logs",groupname,"{}.json".format(instanceid)))
+        log_content = log_object.get()['Body'].read().decode('utf-8')
+        logdict = json.loads(log_content)
+        print(logdict)
+        assert logdict["start"] is not None
+        assert logdict["end"] is not None
+
+    except ClientError as e: 
+        print(e)
+        assert 0
+
+
+
+    
+
 
 
     
