35d4049d560f1db4840e117c3885721f0dcded8d
cellistigs <ta2507@columbia.edu>
2020-04-29 16:24:59 -0400

    automatic commit: deployed pipeline 'testingrandomly'

--- ncap_iac/ncap_blueprints/utils_stack/initialize_neurocaas.sh
+++ ncap_iac/ncap_blueprints/utils_stack/initialize_neurocaas.sh
@@ -6,9 +6,9 @@ aws cloudformation update-stack --stack-name testutilsstack --template-body file
 
 aws cloudformation wait stack-update-complete --stack-name testutilsstack
 
-ssmstackoutput=$(aws cloudformation describe-stack-resources --stack-name testutilsstack | jq '.StackResources[] | select(.LogicalResourceId == "SSMRole") | .PhysicalResourceId')
-lambdastackoutput=$(aws cloudformation describe-stack-resources --stack-name testutilsstack | jq '.StackResources[] | select(.LogicalResourceId == "LambdaRole") | .PhysicalResourceId')
-ec2stackoutput=$(aws cloudformation describe-stack-resources --stack-name testutilsstack | jq '.StackResources[] | select(.LogicalResourceId == "EC2Role") | .PhysicalResourceId')
+ssmstackoutput=$(aws cloudformation describe-stack-resources --stack-name testutilsstack | jq '.StackResources[] | select(.LogicalResourceId == "SSMRole") | .PhysicalResourceId' | sed 's/\"//g')
+lambdastackoutput=$(aws cloudformation describe-stack-resources --stack-name testutilsstack | jq '.StackResources[] | select(.LogicalResourceId == "LambdaRole") | .PhysicalResourceId' | sed 's/\"//g')
+ec2stackoutput=$(aws cloudformation describe-stack-resources --stack-name testutilsstack | jq '.StackResources[] | select(.LogicalResourceId == "EC2Role") | .PhysicalResourceId' | sed 's/\"//g')
 
 
 ## Now do the other stack:
@@ -17,8 +17,8 @@ aws cloudformation update-stack --stack-name testsgstack --template-body file://
 #aws cloudformation wait stack-update-complete --stack-name testsgstack
 
 ## get the names of the security groups you made here
-sgdevoutput=$(aws cloudformation describe-stack-resources --stack-name testsgstack | jq '.StackResources[] | select(.LogicalResourceId == "SecurityGroupDev") | .PhysicalResourceId')
-sgdeployoutput=$(aws cloudformation describe-stack-resources --stack-name testsgstack | jq '.StackResources[] | select(.LogicalResourceId == "SecurityGroupDeploy") | .PhysicalResourceId')
+sgdevoutput=$(aws cloudformation describe-stack-resources --stack-name testsgstack | jq '.StackResources[] | select(.LogicalResourceId == "SecurityGroupDev") | .PhysicalResourceId' | sed 's/\"//g')
+sgdeployoutput=$(aws cloudformation describe-stack-resources --stack-name testsgstack | jq '.StackResources[] | select(.LogicalResourceId == "SecurityGroupDeploy") | .PhysicalResourceId' | sed 's/\"//g')
 
 jq --arg lstack $lambdastackoutput --arg sstack $ssmstackoutput --arg estack $ec2stackoutput --arg sgdev $sgdevoutput --arg sgdeploy $sgdeployoutput '. + {"lambdarolename":$lstack,"ssmrolename":$sstack,"ec2rolename":$estack,"securitygroupdevname":$sgdev,"securitygroupdeployname":$sgdeploy}' ../../global_params.json > ../../global_params_initialized.json
 
