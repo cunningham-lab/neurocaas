c37a7eb7fdd56052389ebf6b359a0367d685a6f9
cellistigs <ta2507@columbia.edu>
2020-05-11 16:40:28 -0400

    integrated testing with setup.

--- tests/initialize_test_user.py
+++ tests/initialize_test_user.py
@@ -4,13 +4,19 @@ import sys
 
 if __name__ == "__main__":
     dirname = sys.argv[1]
+    groupname = sys.argv[2]
+    usernames = sys.argv[3:]
+    assert type(dirname == str)
+    assert type(groupname == str)
+    assert type(usernames == list)
+
 
     with open(os.path.join(dirname,"user_config_template.json"),'r') as f:
         d = json.load(f)
     print(d["UXData"]["Affiliates"][0])
     d["UXData"]["Affiliates"] = [d["UXData"]["Affiliates"][0]]
-    d["UXData"]["Affiliates"][0]["AffiliateName"] = "traviscitestgroup"
-    d["UXData"]["Affiliates"][0]["UserNames"] = ["ciuser1","ciuser2"] 
+    d["UXData"]["Affiliates"][0]["AffiliateName"] = groupname
+    d["UXData"]["Affiliates"][0]["UserNames"] = usernames 
     print(d)
 
     with open(os.path.join(dirname,"user_config_template.json"),'w') as f:
