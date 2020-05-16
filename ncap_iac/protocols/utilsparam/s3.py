f3788b045c38a355d5bd3e23a2a9152b8586ffb8
cellistigs <ta2507@columbia.edu>
2020-05-16 02:42:38 -0400

    made logs more readable.

--- ncap_iac/protocols/utilsparam/s3.py
+++ ncap_iac/protocols/utilsparam/s3.py
@@ -386,7 +386,7 @@ class JobLogger_demo(Logger):
     def append(self, string):
         """ """
         self._logs.append(
-                string + " [log time: {}]".format(str(datetime.datetime.now())[:-4]) 
+                string + "\n\t\t\t [log time: {}]".format(str(datetime.datetime.now())[:-4]) 
         )
     def printlatest(self):
         """ print the most recent item appended to logs. """
