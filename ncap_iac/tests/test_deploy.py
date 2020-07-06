f3cbc56bb6e8f9ff73b13fb9ce518cdb4171cdba
cellistigs <ta2507@columbia.edu>
2020-07-06 13:32:23 -0400

    cleaning up.

--- ncap_iac/tests/test_deploy.py
+++
@@ -1,8 +0,0 @@
-"""
-tests:
-    All resources exist as you have named them. 
-    Users can access resources they should be able to, cannot access those they shouldn't 
-    Users can upload and download all resources appropriately. 
-    Triggers lambda correctly. 
-
-"""
