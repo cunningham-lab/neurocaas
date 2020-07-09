ce029194d5d23001a76eea29bacb5ade8ed302af
cellistigs <ta2507@columbia.edu>
2020-07-09 18:30:27 -0400

    added all tests for basic s3 interface.

--- tests/unit_tests/test_jobmanager_utils.py
+++ tests/unit_tests/test_jobmanager_utils.py
@@ -6,9 +6,10 @@ import ncap_iac.protocols.utilsparam.s3 as s3
 import pytest 
 from botocore.stub import Stubber
 import json
+import yaml
 from unittest.mock import patch,MagicMock,call
-from .test_fixtures import return_json_read, return_json_malformed
-from .test_s3_mock_resources import make_mock_bucket,make_mock_object
+from .test_fixtures import return_json_read, return_json_malformed,return_yaml_read,return_yaml_malformed
+from .test_s3_mock_resources import make_mock_bucket,make_mock_object,make_mock_file_object
 
 
 @pytest.fixture(autouse=True)
@@ -28,7 +29,10 @@ def create_good_submission_args():
         time = record["eventTime"]
     return bucket_name,key,time 
 
-class Test_s3():
+class Test_s3_base():
+    """ Tests the basic functions used to interact directly with the boto3 api. 
+
+    """
     def test_mkdir_obj_exists(self,s3_stub):
         bucket_name = "example_bucket"
         path = "foo"
@@ -168,7 +172,7 @@ class Test_s3():
             assert list(tuple_arguments) == s3.ls_name("bucketname",path)
             mock_bucket_method.assert_called_once()
             
-    def test_ls_name_prefix(self,s3_stub):
+    def test_ls_name_prefix(self):
         tuple_arguments = ("path/to/1","path/to/2","other/path")
         path = "path"
         mbucket = make_mock_bucket([{"key":path} for path in tuple_arguments],filter_path = path)
@@ -221,14 +225,136 @@ class Test_s3():
 
     def test_load_json(self):
         a = return_json_read()
-        file_object_attrs = {'get.return_value':{'Body':MagicMock({**{'read.return_value':a}})}}
-        file_object_mock = MagicMock(**file_object_attrs)
-        print(file_object_mock.get()["Body"].read())
+        file_object_mock = make_mock_file_object(a)
         with patch.object(s3.s3_resource,"Object",return_value = file_object_mock) as mock_fileobj:
+            assert s3.load_json("a","b") == json.loads(a.decode("utf-8"))
 
-            s3.load_json("a","b")
+    def test_load_malformed_json(self):
+        a = return_json_malformed()
+        file_object_mock = make_mock_file_object(a)
+        with patch.object(s3.s3_resource,"Object",return_value = file_object_mock) as mock_fileobj:
+            with pytest.raises(ValueError):
+                 assert s3.load_json("a","b")
+
+    def test_load_yaml(self):
+        a = return_yaml_read()
+        file_object_mock = make_mock_file_object(a)
+        with patch.object(s3.s3_resource,"Object",return_value = file_object_mock) as mock_fileobj:
+            assert s3.load_yaml("a","b") == yaml.safe_load(a.decode("utf-8"))
+
+    def test_load_malformed_yaml(self):
+        a = return_yaml_malformed()
+        file_object_mock = make_mock_file_object(a)
+        with patch.object(s3.s3_resource,"Object",return_value = file_object_mock) as mock_fileobj:
+            with pytest.raises(ValueError):
+                 assert s3.load_yaml("a","b")
+
+    def test_extract_files(self):
+        tuple_arguments = ("path/to/1","path/to/2")
+        path = "path"
+        mbucket = make_mock_bucket([{"key":path} for path in tuple_arguments],filter_path = path)
+        with patch.object(s3.s3_resource,"Bucket",return_value = mbucket) as mock_bucket_method:
+            assert list(tuple_arguments) == s3.extract_files("bucketname",path)
+            mock_bucket_method.assert_called_once()
+
+    def test_extract_files_with_directories(self):
+        tuple_arguments = ("path/to/1","path/to","path/to/2","path/to/")
+        path = "path"
+        mbucket = make_mock_bucket([{"key":path} for path in tuple_arguments],filter_path = path)
+        with patch.object(s3.s3_resource,"Bucket",return_value = mbucket) as mock_bucket_method:
+            assert list(tuple_arguments)[:-1] == s3.extract_files("bucketname",path)
+            mock_bucket_method.assert_called_once()
+
+    def test_extract_files_with_extensions(self):
+        tuple_arguments = ("path/to/1.jpg","path/to/2","path/to/")
+        path = "path"
+        mbucket = make_mock_bucket([{"key":path} for path in tuple_arguments],filter_path = path)
+        with patch.object(s3.s3_resource,"Bucket",return_value = mbucket) as mock_bucket_method:
+            assert list(tuple_arguments)[:1] == s3.extract_files("bucketname",path,ext = "jpg")
+            mock_bucket_method.assert_called_once()
+
+    def test_extract_files_with_malformed_extensions(self):
+        tuple_arguments = ("path/to/1.jpg","path/to/2","path/to/")
+        path = "path"
+        mbucket = make_mock_bucket([{"key":path} for path in tuple_arguments],filter_path = path)
+        with patch.object(s3.s3_resource,"Bucket",return_value = mbucket) as mock_bucket_method:
+            with pytest.raises(ValueError):
+                assert s3.extract_files("bucketname",path,ext = ".jpg")
+            mock_bucket_method.assert_called_once()
+
+    def test_write_endfile(self):
+        mbucket = make_mock_bucket([]) 
+        with patch.object(s3.s3_resource,"Bucket",return_value=mbucket) as mock_bucket_method:
+            s3.write_endfile("bucketname","resultpath")
+            assert (mock_bucket_method.mock_calls) == [call('bucketname')]
+            assert (mock_bucket_method.return_value.mock_calls) == [call.put_object(Body=b'end of analysis marker', Key='resultpath/process_results/end.txt')] 
+
+    def test_write_active_monitorlog(self):
+        mbucket = make_mock_bucket([]) 
+        with patch.object(s3.s3_resource,"Bucket",return_value=mbucket) as mock_bucket_method:
+            s3.write_active_monitorlog("bucketname","resultpath",{"key":"value"})
+            assert (mock_bucket_method.mock_calls) == [call('bucketname')]
+            print((mock_bucket_method.return_value.mock_calls))
+            assert (mock_bucket_method.return_value.mock_calls) == [call.put_object(Body=b'{\n  "key": "value"\n}', Key='logs/active/resultpath')] 
+
+    def test_delete_active_monitorlog(self,s3_stub):
+        bucketname = "bucketname"
+        key = "key"
+        s3_stub.add_response("delete_object",expected_params={"Bucket":bucketname,"Key":os.path.join("logs","active",key)},service_response = {})
+        s3.delete_active_monitorlog(bucketname,key)
+
+    def test_update_monitorlog_start(self):
+        time = "t0"
+        log_init = {"start":"init_val","end":"init_val"}
+        instance_id = "i-12345" 
+        a = return_yaml_read()
+        mbucket = make_mock_bucket([]) 
+        file_object_mock = make_mock_file_object(a)
+        mbucket.put_object.return_value = file_object_mock
+        with patch.object(s3.s3_resource,"Bucket",return_value=mbucket) as mock_bucket_method:
+            with patch('ncap_iac.protocols.utilsparam.s3.load_json',return_value=log_init) as mock_fileobj_method:
+                log_test = {"start":time,"end":"init_val"}
+                s3.update_monitorlog("bucketname",instance_id,"running",time)
+                print((mock_bucket_method.return_value.put_object.mock_calls))
+                assert (mock_bucket_method.mock_calls) == [call('bucketname')]
+                assert (mock_bucket_method.return_value.mock_calls) == [call.put_object(Body=bytes(json.dumps(log_test,indent = 2).encode('UTF-8')), Key=os.path.join("logs","active",instance_id))] 
+        
+    def test_update_monitorlog_start(self):
+        time = "t0"
+        log_init = {"start":"init_val","end":"init_val"}
+        instance_id = "i-12345" 
+        a = return_yaml_read()
+        mbucket = make_mock_bucket([]) 
+        file_object_mock = make_mock_file_object(a)
+        mbucket.put_object.return_value = file_object_mock
+        with patch.object(s3.s3_resource,"Bucket",return_value=mbucket) as mock_bucket_method:
+            with patch('ncap_iac.protocols.utilsparam.s3.load_json',return_value=log_init) as mock_fileobj_method:
+                log_test = {"start":"init_val","end":time}
+                s3.update_monitorlog("bucketname",instance_id,"shutting-down",time)
+                print((mock_bucket_method.return_value.put_object.mock_calls))
+                assert (mock_bucket_method.mock_calls) == [call('bucketname')]
+                assert (mock_bucket_method.return_value.mock_calls) == [call.put_object(Body=bytes(json.dumps(log_test,indent = 2).encode('UTF-8')), Key=os.path.join("logs","active",instance_id))] 
+
+
+## Todo: cover exception. 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
