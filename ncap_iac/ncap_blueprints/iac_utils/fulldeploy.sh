dd547c856e67cee82f744295abdfdf5b00f5d510
cellistigs <ta2507@columbia.edu>
2020-07-23 12:39:57 -0400

    saving to master branch

--- ncap_iac/ncap_blueprints/iac_utils/fulldeploy.sh
+++ ncap_iac/ncap_blueprints/iac_utils/fulldeploy.sh
@@ -38,7 +38,7 @@ sam build -t compiled_template.json -m "$ncaprootdir"/protocols/requirements.txt
 
 sam package --s3-bucket $storagebucketname --output-template-file compiled_packaged.yaml
 
-sam deploy --template-file compiled_packaged.yaml --stack-name $PIPENAME --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+sam deploy --template-file compiled_packaged.yaml --stack-name $PIPENAME --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --s3-bucket $storagebucketname
 
 #aws s3 cp test_resources/computereport_1234567.json s3://$PIPENAME/logs/debug/
 #aws s3 cp test_resources/computereport_2345678.json s3://$PIPENAME/logs/debug/
--- ncap_iac/protocols/helper.py
+++ ncap_iac/protocols/helper.py
@@ -4,16 +4,24 @@ try:
     import json 
     import traceback
     from submit_start import respond
-    import utils.s3
-    import utils.ssm
-    import utils.ec2
-    import utils.serverless
+    import utilsparam.s3
+    import utilsparam.ssm
+    import utilsparam.ec2
+    import utilsparam.serverless
 except Exception as e:
-    error = str(e)
-    stacktrace = json.dumps(traceback.format_exc())
-    message = "Exception: " + error + "  Stacktrace: " + stacktrace
-    err = {"message": message}
-    print(err)
+    try:
+        ## Most likely this comes from pytest and relative imports.
+        from ncap_iac.protocols.utilsparam import s3 as utilsparams3
+        from ncap_iac.protocols.utilsparam import ssm as utilsparamssm
+        from ncap_iac.protocols.utilsparam import ec2 as utilsparamec2
+        from ncap_iac.protocols.utilsparam import events as utilsparamevents
+        from ncap_iac.protocols.utilsparam import pricing as utilsparampricing
+    except:
+        error = str(e)
+        stacktrace = json.dumps(traceback.format_exc())
+        message = "Exception: " + error + "  Stacktrace: " + stacktrace
+        err = {"message": message}
+        print(err)
 
 def handler_mkdir(event,context):
     """The function corresponding to a cloudformation lambda-backed custom resource that creates new directories in S3 buckets. 
@@ -38,14 +46,14 @@ def handler_mkdir(event,context):
             path = props['Path']
             dirname = props['DirName']
             ## Now plug in:
-            utils.s3.mkdir(bucket,path,dirname)
+            utilsparam.s3.mkdir(bucket,path,dirname)
         else:
             print(event['RequestType'])
-        utils.serverless.sendResponse(event,context,"SUCCESS",responseData)
+        utilsparam.serverless.sendResponse(event,context,"SUCCESS",responseData)
 
     except Exception as e:
         error = str(e)
-        utils.serverless.sendResponse(event,context,"FAILED",{"data":traceback.format_exc()})
+        utilsparam.serverless.sendResponse(event,context,"FAILED",{"data":traceback.format_exc()})
         #stacktrace = json.dumps(traceback.format_exc())
         #message = "Exception: " + error + "  Stacktrace: " + stacktrace
         #err = {"message": message}
@@ -71,12 +79,15 @@ def handler_deldir(event, context):
             ## Get individual properties: 
             bucket = props['BucketName']
             path = props['Path']
-            utils.s3.deldir(bucket,path)
+            dirname = props["DirName"]
+            ## Important, we want to delete the directory, not its parents. 
+            fullpath = os.path.join(path,dirname)
+            utilsparam.s3.deldir(bucket,fullpath)
 
-        utils.serverless.sendResponse(event,context,"SUCCESS",responseData)
+        utilsparam.serverless.sendResponse(event,context,"SUCCESS",responseData)
     except Exception as e:
         error = str(e)
-        utils.serverless.sendResponse(event,context,"FAILED",{"data":traceback.format_exc()})
+        utilsparam.serverless.sendResponse(event,context,"FAILED",{"data":traceback.format_exc()})
 
 def handler_delbucket(event, context):
     """The function corresponding to a cloudformation lambda-backed custom resource that deletes the whole contents of an S3 bucket. Necessary to delete s3 buckets when deleting cloudformation stacks. 
@@ -95,11 +106,11 @@ def handler_delbucket(event, context):
     try:
         if event['RequestType'] == 'Delete':
             bucket = event['ResourceProperties']['BucketName']
-            s3 = utils.s3.delbucket(bucket)
+            s3 = utilsparam.s3.delbucket(bucket)
 
-        utils.serverless.sendResponse(event,context,"SUCCESS",responseData)
+        utilsparam.serverless.sendResponse(event,context,"SUCCESS",responseData)
     except Exception as e:
         error = str(e)
-        utils.serverless.sendResponse(event,context,"FAILED",{"data":traceback.format_exc()})
+        utilsparam.serverless.sendResponse(event,context,"FAILED",{"data":traceback.format_exc()})
 
 
