0ee3e4b9eaff20572c5454693780cf2f61423eca
cellistigs <ta2507@columbia.edu>
2020-07-29 22:12:29 -0400

    added region.

--- tests/create_profile.sh
+++ tests/create_profile.sh
@@ -4,7 +4,9 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+region=us-east-1
+output=json
 EOL
 
