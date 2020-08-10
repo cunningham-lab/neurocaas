20b61d1df73b1cd64b313b70c6af380e9c076095
cellistigs <ta2507@columbia.edu>
2020-08-10 14:19:15 -0400

    changes to dev builder.

--- ncap_iac/utils/dev_builder.py
+++ ncap_iac/utils/dev_builder.py
@@ -6,6 +6,7 @@ from troposphere.awslambda import Permission
 from troposphere.logs import LogGroup
 from troposphere.cloudformation import CustomResource,Stack 
 from lambda_policies import lambda_basepolicy,lambda_writeS3
+
 import sys
 import json 
 import subprocess
