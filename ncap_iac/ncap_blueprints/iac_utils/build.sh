6dc7e61d2a431f489304a40aeebcdfd75fd455a5
cellistigs <ta2507@columbia.edu>
2020-08-19 12:57:31 -0400

    removed conda environment start + template build.

--- ncap_iac/ncap_blueprints/iac_utils/build.sh
+++ ncap_iac/ncap_blueprints/iac_utils/build.sh
@@ -7,7 +7,7 @@ ncaprootdir="$(dirname "$(dirname "$scriptdir")")"
 source "$scriptdir"/paths.sh
 ## Get the path to this particular file. 
 ## NOTE: Add the anaconda path if running as admin.  
-source activate neurocaas
+#source activate neurocaas
 
 ## Input management: 
 ## Get the path to the directory where user data is stored: 
@@ -29,7 +29,7 @@ cd $ncaprootdir/utils
 stage=$(jq ".STAGE" "$PIPEDIR"/stack_config_template.json ) 
 stagestr=$(echo $stage | tr -d '"')
 echo $stagestr
-python dev_builder.py $PIPEDIR/stack_config_template.json "$stagestr"
+#python dev_builder.py $PIPEDIR/stack_config_template.json "$stagestr"
 
 ## We need to navigate to the pipeline directory because we have a relative path in our compilation code. 
 cd $PIPEDIR
