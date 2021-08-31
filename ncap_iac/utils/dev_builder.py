704d2b1d938148ed1e22d4b49ee2380193144a4b
cellistigs <ta2507@columbia.edu>
2021-08-31 16:02:18 -0400

    forgot security group as option

--- ncap_iac/utils/dev_builder.py
+++ ncap_iac/utils/dev_builder.py
@@ -100,16 +100,19 @@ class NeuroCaaSTemplate(object):
                 'OUTDIR',
                 'LOGDIR',
                 "CONFIGDIR",
-                "SUBMITDIR"]
+                "SUBMITDIR",
+                "SECURITY_GROUPS"]
         vals = [gpdict["input_directory"],
                 gpdict["output_directory"],
                 gpdict["log_directory"],
                 gpdict["config_directory"],
-                gpdict["submission_directory"]]
+                gpdict["submission_directory"],
+                gpdict["securitygroupdeployname"]]
+
         appenddict = {k:v for k,v in zip(keys,vals)}
         obj['Lambda']['LambdaConfig'].update(appenddict)
         ## Now append the other fields: 
-        default_fixed = ["EXECUTION_TIMEOUT","SSM_TIMEOUT","LAUNCH","LOGFILE","DEPLOY_LIMIT","MAXCOST","MONITOR"]
+        default_fixed = ["WORKING_DIRECTORY","CONFIG","MISSING_CONFIG_ERROR","SHUTDOWN_BEHAVIOR","EXECUTION_TIMEOUT","SSM_TIMEOUT","LAUNCH","LOGFILE","DEPLOY_LIMIT","MAXCOST","MONITOR"]
         for field in default_fixed:
             if not obj["Lambda"]["LambdaConfig"].get(field,False):
                 obj["Lambda"]["LambdaConfig"][field] = gpdict[field]
